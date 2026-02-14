f75081075e9c80a8af3fbb1fc53fe3ba6ac2d113
José Jácome <josejacomeb@gmail.com>
2026-02-13 20:31:39 -0500

    arreglo: nombres mas familiar de los productos

--- app/catalogo/admin.py
+++ app/catalogo/admin.py
@@ -5,5 +5,5 @@ from django.contrib import admin
 # Register your models here.
 @admin.register(ProductoBase)
 class CatalogoAdmin(admin.ModelAdmin):
-    list_display = ("nombre", "precio_publico_defecto", "precio_interno_defecto")
+    list_display = ("nombre", "precio_publico", "costo")
     search_fields = ("nombre",)  # Necessary for autocomplete
--- app/catalogo/models.py
+++ app/catalogo/models.py
@@ -14,9 +14,8 @@ class ProductoBase(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
 
     # Estos son los precios 'sugeridos' que se copiarán a la campaña
-    precio_interno_defecto = models.DecimalField(max_digits=10, decimal_places=2)
-    precio_publico_defecto = models.DecimalField(max_digits=10, decimal_places=2)
+    costo = models.DecimalField(max_digits=10, decimal_places=2)
+    precio_publico = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"{self.nombre}"
-
