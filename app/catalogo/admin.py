f39ac7242897e615e209c0c3a5b4a87e2b701007
José Jácome <josejacomeb@gmail.com>
2026-02-19 13:24:35 -0500

    tarea: catalogo de productos y servicios - Manejar los productos y servicios por campaña en un catálogo

--- app/catalogo/admin.py
+++ app/catalogo/admin.py
@@ -1,9 +1,16 @@
-from catalogo.models import ProductoBase
+from catalogo.models import Producto, Servicio
 from django.contrib import admin
 
 
-# Register your models here.
-@admin.register(ProductoBase)
-class CatalogoAdmin(admin.ModelAdmin):
-    list_display = ("nombre", "precio_publico", "costo")
-    search_fields = ("nombre",)  # Necessary for autocomplete
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "default_precio_venta", "default_precio_compra", "tipo_medicina")
+    search_fields = ("nombre", "tipo_medicina")
+    list_filter = ("tipo_medicina",)
+
+
+@admin.register(Servicio)
+class ServicioAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "default_precio_publico", "default_costo_veterinario", "categoria")
+    search_fields = ("nombre", "categoria")
+    list_filter = ("categoria",)
